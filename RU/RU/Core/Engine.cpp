7b4c3bf3e3768dd105f6c121a8e07b90eb54c500
Zayd Qureshi <zayd.qureshi9@gmail.com>
2021-12-12 18:48:50 -0500

    fix go

--- RU/RU/Core/Engine.cpp
+++ RU/RU/Core/Engine.cpp
@@ -49,7 +49,7 @@ bool Engine::Run()
 	scene.vecGameObjects.push_back(player);
 
 
-	
+	bool go = false;
 
 	while (isRunning)
 	{
@@ -57,7 +57,11 @@ bool Engine::Run()
 		isRunning = !Input::Get()->quit;
 		ts.SetDeltaTime();
 
-		scene.Update(ts.GetDeltaTime());
+		if (Input::Get()->isKeyDown(SDL_SCANCODE_B))
+			go = !go;
+
+		if(go)
+			scene.Update(ts.GetDeltaTime());
 
 	
 
